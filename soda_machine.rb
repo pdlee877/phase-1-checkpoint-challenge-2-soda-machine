b8908b8f557f0e0249e5ed08e328bd0e2c12283f
Paula Lee <pdlee877@gmail.com>
2016-08-29 15:00:25 -0700

    paula lee checkpoint challenge 2

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,22 @@
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_reader :sodas, :cash, :brand
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @brand = brand
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    p soda_brand.brand
   end
 
   def sell(soda_brand)
+
   end
 
 end
